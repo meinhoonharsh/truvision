a85388f5fa62efd6e8c51da9b8e1cecc36eb3c9c
meinhoonharsh <harshprogrammer782@gmail.com>
2023-08-23 02:17:13 +0530

    Fixed Contact and FAQs

---
+++ resources/views/sections/homecontact.blade.php
@@ -0,0 +1,25 @@
+<style>
+    .contentdiv p {
+        font-family: var(--ptext);
+        font-size: 1.5em;
+        color: #fffc;
+        text-align: justify;
+        word-spacing: 0.3;
+    }
+</style>
+@include('mini.parallax', ['name' => 'Contact Us', 'random' => rand(1, 18)])
+
+<div class="contentdiv">
+    <div class="container-80">
+        <p>
+            Just drop us the dates, the events included for each day , tentative crowd count , tentative pheras timing ,
+            the style of wedding (maharashtrian, gujrati etc. ) location and property name . This helps us in forming
+            the team structure and giving you a reasonable quote accordingly.
+
+            We typically drop the quotation mail within 48 hrs of any enquiry.
+        </p>
+        <center>
+            @include('mini.anchor', ['name' => 'Contact Us'])
+        </center>
+    </div>
+</div>
