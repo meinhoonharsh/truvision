32a33d5c3b0cf101a1f9de3bc67376c56b7b9f87
meinhoonharsh <harshprogrammer782@gmail.com>
2023-08-23 00:58:26 +0530

    first commit

---
+++ static/boto/js/main.js
@@ -0,0 +1,58 @@
+/* ===================================
+--------------------------------------
+	Boto | Photography HTML Template
+	Version: 1.0
+	Copyright By: ColorLib
+--------------------------------------
+======================================*/
+
+'use strict';
+(function($) {
+
+	/*-------------------
+		Hero Slider
+	-------------------*/
+	$('.hero-slider').slick({
+		dots: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		centerMode: true,
+		variableWidth: true,
+		centerMode: true,
+		arrows: false,
+		asNavFor: '.hero-text-slider',
+		autoplay: true,
+		pauseOnHover:false,
+		autoplaySpeed: 3000
+	});
+	
+	var hero_slider = $('.hero-slider');
+
+	hero_slider.on('wheel', (function(e) {
+		e.preventDefault();
+		if (e.originalEvent.deltaY < 0) {
+			$(this).slick('slickPrev');
+		} else {
+			$(this).slick('slickNext');
+		}
+	}));
+
+	hero_slider.on('click', '.slick-slide', function (e) {
+		e.preventDefault();
+		var index = $(this).data("slick-index");
+		if ($('.slick-slider').slick('slickCurrentSlide') !== index) {
+			$('.slick-slider').slick('slickGoTo', index);
+		}
+	});
+
+	$('.hero-text-slider').slick({
+		dots: false,
+		infinite: false,
+		speed: 300,
+		arrows: false,
+		asNavFor: '.hero-slider',
+	});
+
+})(jQuery);
+
