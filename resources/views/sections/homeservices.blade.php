6f9aa2ca9cf2fa9b20d501a98ba5616298c4dc39
meinhoonharsh <harshprogrammer782@gmail.com>
2024-11-16 01:13:39 +0530

    Added Icons to Services

--- resources/views/sections/homeservices.blade.php
+++ resources/views/sections/homeservices.blade.php
@@ -1,121 +1,121 @@
 @php
     $services = [
-    [
-        'title' => 'Customize Gifting',
-        'features' => [
-            'Personalized photo gifts',
-            'Unique designs for special occasions',
-            'Custom frames and keepsakes',
-            'Wide range of gift options',
+        [
+            'title' => 'Customize Gifting',
+            'features' => [
+                'Personalized photo gifts',
+                'Unique designs for special occasions',
+                'Custom frames and keepsakes',
+                'Wide range of gift options',
+            ],
+            'image' => '/static/assets/images/services/1.svg',
         ],
-        'image' => 'path/to/customize-gifting.jpg'
-    ],
-    [
-        'title' => 'Wedding Shoots',
-        'features' => [
-            'Professional wedding photography',
-            'Engagement and pre-wedding shoots',
-            'Candid and traditional styles',
-            'High-quality photo and video packages',
+        [
+            'title' => 'Wedding Shoots',
+            'features' => [
+                'Professional wedding photography',
+                'Engagement and pre-wedding shoots',
+                'Candid and traditional styles',
+                'High-quality photo and video packages',
+            ],
+            'image' => '/static/assets/images/services/2.svg',
         ],
-        'image' => 'path/to/wedding-shoots.jpg'
-    ],
-    [
-        'title' => 'Event Photography',
-        'features' => [
-            'Coverage for corporate and social events',
-            'Expert photographers and videographers',
-            'Fast turnaround on event photos',
-            'On-site printing and digital copies available',
+        [
+            'title' => 'Event Photography',
+            'features' => [
+                'Coverage for corporate and social events',
+                'Expert photographers and videographers',
+                'Fast turnaround on event photos',
+                'On-site printing and digital copies available',
+            ],
+            'image' => '/static/assets/images/services/3.svg',
         ],
-        'image' => 'path/to/event-photography.jpg'
-    ],
-    [
-        'title' => 'Printing',
-        'features' => [
-            'Custom photo albums',
-            'High-quality poster prints',
-            'Canvas and fine art printing',
-            'Options for sizes and finishes',
+        [
+            'title' => 'Printing',
+            'features' => [
+                'Custom photo albums',
+                'High-quality poster prints',
+                'Canvas and fine art printing',
+                'Options for sizes and finishes',
+            ],
+            'image' => '/static/assets/images/services/4.svg',
         ],
-        'image' => 'path/to/printing.jpg'
-    ],
-];
+    ];
 
 @endphp
 <style>
-    
-:root {
-  /* --primary: #631A51; 
+    :root {
+        /* --primary: #631A51;
   --secondary: #992959;*/
-  --primary: #061621;
-  --secondary: #0d3149;
-  --orange: #FFB250;
-  --green: #85A562;
-  --blue: #3D5189;
+        --primary: #061621;
+        --secondary: #0d3149;
+        --orange: #FFB250;
+        --green: #85A562;
+        --blue: #3D5189;
 
 
-  --royal: blackchancery;
-  /* --ptext:'Lobster Two', cursive; */
-  --ptext:'roboto';
-  --philosopher: 'Philosopher', sans-serif;
-}
+        --royal: blackchancery;
+        /* --ptext:'Lobster Two', cursive; */
+        --ptext: 'roboto';
+        --philosopher: 'Philosopher', sans-serif;
+    }
 
-.contentdiv {
-  padding: 50px 0;
-}
+    .contentdiv {
+        padding: 50px 0;
+    }
 
-.servicebox {
-  background: var(--primary);
-  padding: 20px;
-  border-radius: 5px;
-  box-shadow: 0 0 3px var(--orange);
-  margin-bottom: 20px;
-  transition: all 0.3s;
-}
+    .servicebox {
+        background: var(--primary);
+        padding: 20px;
+        border-radius: 5px;
+        box-shadow: 0 0 3px var(--orange);
+        margin-bottom: 20px;
+        transition: all 0.3s;
+    }
 
-.servicebox:hover {
-  box-shadow: 0 0 5px var(--orange);
-}
+    .servicebox:hover {
+        box-shadow: 0 0 5px var(--orange);
+    }
 
+    .serviceimg {
+        text-align: center;
+    }
 
-.serviceimg img {
-  width: 100%;
-  min-height: 100px;
-  aspect-ratio: 16/9;
-}
+    .serviceimg img {
+        width: 50%;
+        margin: 0 auto;
+        min-height: 50px;
+        aspect-ratio: 16/9;
+    }
 
-.servicetitle h3 {
-  color: var(--orange);
-  font-family: var(--philosopher);
-  font-size: 1.4em;
-  text-align: center;
-  margin: 1em 0;
-}
+    .servicetitle h3 {
+        color: var(--orange);
+        font-family: var(--philosopher);
+        font-size: 1.4em;
+        text-align: center;
+        margin: 1em 0;
+    }
 
-.servicefeatures ul {
-  list-style: none;
-  padding: 0;
-  text-align: center;
-}
+    .servicefeatures ul {
+        list-style: none;
+        padding: 0;
+        text-align: center;
+    }
 
-.servicefeatures ul li {
-  color: white;
-  font-family: var(--ptext);
-  opacity: 0.8;
-  font-size: 1em;
-  text-align: center;
-  margin: 0.5em 0;
-}
-
-
-    
+    .servicefeatures ul li {
+        color: white;
+        font-family: var(--ptext);
+        opacity: 0.8;
+        font-size: 1em;
+        text-align: center;
+        margin: 0.5em 0;
+    }
 </style>
 @include('mini.parallax', ['name' => 'Services', 'random' => rand(1, 18)])
 
 <div class="contentdiv container" id="services">
     <div class="row">
-        @foreach($services as $service)
+        @foreach ($services as $service)
             <div class="col-md-3">
                 <div class="servicebox">
                     <div class="serviceimg">
@@ -126,7 +126,7 @@
                     </div>
                     <div class="servicefeatures">
                         <ul>
-                            @foreach($service['features'] as $feature)
+                            @foreach ($service['features'] as $feature)
                                 <li>{{ $feature }}</li>
                             @endforeach
                         </ul>
@@ -137,5 +137,4 @@
     </div>
 </div>
 
-<script>
-</script>
+<script></script>
